9737dbeaaa3ae16da2fe467d6a35983163a5cdf0
anirudhsriram <anirudhsriram1995@gmail.com>
2018-07-03 13:28:02 +0530

    clean up TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,24 @@ import { EventEmitter } from 'events';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   showList = true;
    selectedBook ;
    newBook ;
    selectedBookIndex ;
    showNew ;
-  // @Input() returnedBook: string;
   // tslint:disable-next-line:max-line-length
   books = ['In Search of Lost Time by Marcel Proust', 'Don Quixote by Miguel de Cervantes', 'Ulysses by James Joyce', ' The Great Gatsby by F. Scott Fitzgerald', 'Moby Dick by Herman Melville', 'Hamlet by William Shakespeare'] ;
+  // Function to pass data to child and remember index on the array to update list once child submits event//
   onselectedBook(data: string, index) {
       this.selectedBook = data ;
       this.selectedBookIndex = index ;
-
   }
-  doSomething(data) {
+  // Function to change the edited book on the book list//
+  updateList(data) {
     this.selectedBook = '';
      this.books[this.selectedBookIndex] = data ;
   }
+  // Function that adds new book to the bookArray//
   onAddNew() {
       this.books.push(this.newBook) ;
       this.newBook = '' ;
